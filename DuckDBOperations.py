3c653ce739fe81a228e1a3e24b735d434ad66ea1
d.kandelov <kaparyaka@mail.ru>
2023-12-16 21:23:30 +0300

    Add convenient output in DuckDBOperations

--- DuckDBOperations.py
+++ DuckDBOperations.py
@@ -2,28 +2,34 @@ import duckdb
 from time import time
 
 
-def DuckDBFirstQuery(data):
-    duckdb.sql("SELECT VendorID, count(*) FROM data GROUP BY 1;")
+def DuckDBFirstQuery(data, prints):
+    result = duckdb.sql("SELECT VendorID, count(*) FROM data GROUP BY 1;")
+    if prints:
+        result.show()
 
 
-def DuckDBSecondQuery(data):
-    duckdb.sql("""SELECT passenger_count, avg(total_amount) 
+def DuckDBSecondQuery(data, prints):
+    result = duckdb.sql("""SELECT passenger_count, avg(total_amount) 
                   FROM data 
                   GROUP BY 1;""")
+    if prints:
+        result.show()
 
 
-def DuckDBThirdQuery(data):
-    duckdb.sql("""SELECT
+def DuckDBThirdQuery(data, prints):
+    result = duckdb.sql("""SELECT
                        passenger_count, 
                        extract(year from tpep_pickup_datetime),
                        count(*)
                    FROM data
                    GROUP BY 1, 2;
                   """)
+    if prints:
+        result.show()
 
 
-def DuckDBFourthQuery(data):
-    duckdb.sql("""SELECT
+def DuckDBFourthQuery(data, prints):
+    result = duckdb.sql("""SELECT
                        passenger_count,
                        extract(year from tpep_pickup_datetime),
                        round(trip_distance),
@@ -32,9 +38,13 @@ def DuckDBFourthQuery(data):
                   GROUP BY 1, 2, 3
                   ORDER BY 2, 4 desc;
                   """)
+    if prints:
+        result.show()
 
 
 def CheckDuckDB(tries, big_data=False):
+    prints = 0
+
     time_sum = 0
     time_first = 0
     time_second = 0
@@ -49,16 +59,16 @@ def CheckDuckDB(tries, big_data=False):
     for i in range(tries):
 
         start_time = time()
-        DuckDBFirstQuery(data)
+        DuckDBFirstQuery(data, prints)
         time_first += time() - start_time
         time1 = time()
-        DuckDBSecondQuery(data)
+        DuckDBSecondQuery(data, prints)
         time_second += time() - time1
         time2 = time()
-        DuckDBThirdQuery(data)
+        DuckDBThirdQuery(data, prints)
         time_third += time() - time2
         time3 = time()
-        DuckDBFourthQuery(data)
+        DuckDBFourthQuery(data, prints)
         time_fourth += time() - time3
         finish_time = time()
 
