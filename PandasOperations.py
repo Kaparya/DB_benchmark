b5a4bcb7854d80f130190781aac717e5de170dd8
d.kandelov <kaparyaka@mail.ru>
2023-12-16 23:14:53 +0300

    Rename functions in PandasOperations

--- PandasOperations.py
+++ PandasOperations.py
@@ -2,7 +2,7 @@ import pandas as pd
 from time import time
 
 
-def PandasFirstRequest(data, prints):
+def PandasFirstQuery(data, prints):
     selected_data = data[['VendorID']]
     grouped_data = selected_data.groupby('VendorID')
     final_data = grouped_data.size().reset_index(name='counts')
@@ -10,7 +10,7 @@ def PandasFirstRequest(data, prints):
         print(final_data)
 
 
-def PandasSecondRequest(data, prints):
+def PandasSecondQuery(data, prints):
     selected_data = data[['passenger_count', 'total_amount']]
     grouped_data = selected_data.groupby('passenger_count')
     final_data = grouped_data.mean().reset_index()
@@ -18,7 +18,7 @@ def PandasSecondRequest(data, prints):
         print(final_data)
 
 
-def PandasThirdRequest(data, prints):
+def PandasThirdQuery(data, prints):
     selected_data = data[['passenger_count', 'tpep_pickup_datetime']]
     selected_data['year'] = pd.to_datetime(
         selected_data.pop('tpep_pickup_datetime'),
@@ -29,7 +29,7 @@ def PandasThirdRequest(data, prints):
         print(final_data)
 
 
-def PandasFourthRequest(data, prints):
+def PandasFourthQuery(data, prints):
     selected_data = data[['passenger_count', 'tpep_pickup_datetime', 'trip_distance']]
     selected_data['trip_distance'] = selected_data['trip_distance'].round().astype(int)
     selected_data['year'] = pd.to_datetime(
@@ -62,16 +62,16 @@ def CheckPandas(tries, big_data=False):
     for i in range(tries):
 
         start_time = time()
-        PandasFirstRequest(data, prints)
+        PandasFirstQuery(data, prints)
         time_first += time() - start_time
         time1 = time()
-        PandasSecondRequest(data, prints)
+        PandasSecondQuery(data, prints)
         time_second += time() - time1
         time2 = time()
-        PandasThirdRequest(data, prints)
+        PandasThirdQuery(data, prints)
         time_third += time() - time2
         time3 = time()
-        PandasFourthRequest(data, prints)
+        PandasFourthQuery(data, prints)
         time_fourth += time() - time3
         finish_time = time()
 
