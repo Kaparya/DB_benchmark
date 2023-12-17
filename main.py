889ef8ba372264b704bb054d3a699ce3c80b3957
d.kandelov <kaparyaka@mail.ru>
2023-12-17 16:22:05 +0300

    Ready to test

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-import psycopg2
-import sqlite3
-import duckdb
-import PandasOperations
-from pandas import read_csv
-import sqlalchemy
+from PandasOperations import CheckPandas
+from DuckDBOperations import CheckDuckDB
+from PsycopgOperations import CheckPostgres
+from SqliteOperations import CheckSqlite
+from SQLAlchemyOperations import CheckSQLAlchemy
 
 
-time_pandas = PandasOperations.CheckPandas(3, False)
+big_data = True
+tries = 3
 
+time_pandas = CheckPandas(tries, big_data)
+print('Pandas')
 print(time_pandas)
+
+time_duckdb = CheckDuckDB(tries, big_data)
+print('DuckDB')
+print(time_duckdb)
+
+time_psycopg = CheckPostgres(tries, big_data)
+print('Postgres')
+print(time_psycopg)
+
+time_sqlite = CheckSqlite(tries, big_data)
+print('SQLite')
+print(time_sqlite)
+
+time_SQLAlchemy = CheckSQLAlchemy(tries, big_data)
+print('SQLAlchemy')
+print(time_SQLAlchemy)
